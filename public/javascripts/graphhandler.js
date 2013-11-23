78b35ee9ab6538bd59daed71a7a0d2761cbda145
Amman Vedi <amman.vedi@gmail.com>
2013-11-23 17:14:09 +0000

    added js code for basic node data structure, plumbing for forms works

--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -1,5 +1,11 @@
 console.log("started handler");
 
+
+$(document).ready(function() {
+
+
+//-----------------------VARIABLES
+
 var meta = {};
 
 //default node creation
@@ -12,9 +18,45 @@ var node = {
 	meta: {},
 }
 
-$(document).ready(function() {
-    $('#btn_addnode').click(function() {
+
+
+var nodes = new Array();
+
+nodes[0] = node;
+
+//-----------------------FUNCTIONS
+
+function showNode(n){
+	return 'NODE ---->   id: ' + n['id'] + ' x: ' + n['posx'] + ' y: ' + n['posy'];
+}
+
+
+function listnodes(nodes){
+	var x = nodes.length;
+	
+	var list = '';
+
+	if (x < 1) {return 'no nodes to list'};
+
+	for (var i = 0; i < x; i++) {
+		var nodeobj = nodes[i];
+		list += showNode(nodeobj) + '<br/>'
+	};
+
+	return list;
+}
+
+//-----------------------DELEGATES
+
+$('#display_code').html(listnodes(nodes));
+
+$('#btn_addnode').click(function() {
       console.log($('#field_nodename').val());
     });
+
+
+
+
+
 });
 
