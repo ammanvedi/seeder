e888f90048594d164240dbb76e44884d1450c95e
Amman Vedi <amman.vedi@gmail.com>
2013-11-23 20:54:15 +0000

    added sigma js integration, adding nodes is functional

--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -1,9 +1,21 @@
 console.log("started handler");
 
 
+
+
+
+
+
+
+
 $(document).ready(function() {
 
+	var sigRoot = document.getElementById('graph_canvas');
+var sigInst = sigma.init(sigRoot);
 
+
+$( "#tabs" ).tabs();
+$("#display_code").tabs();
 //-----------------------VARIABLES
 
 var meta = {};
@@ -12,18 +24,35 @@ var meta = {};
 var node = {
 	posx: 100,
 	posy: 100,
-	size: 2,
+	size: 20,
 	id: 'id',
-	name: '',
+	name: 'initial node',
 }
-
-
 
 var nodes = new Array();
 
 nodes[0] = node;
 
 //-----------------------FUNCTIONS
+
+
+
+function drawGraph(){
+
+
+sigInst.draw();
+
+
+/*  for(i = 1; i < (N-2); i++){
+      var cluster = clusters[(Math.random()*C)|0], n = cluster.nodes.length;
+      sigInst.addEdge(i,allnodes[i],allnodes[i+1]);
+ } */
+
+
+  // Draw the graph :
+
+
+}
 
 function showNode(n){
 	return '-   id: ' + n['id'] + ' name: ' + n['name'] + ' x: ' + n['posx'] + ' y: ' + n['posy'];
@@ -56,12 +85,28 @@ function addNode(x, y, s, id, n){
 
 	nodes.push(newnode);
 
+	sigInst.addNode(id,{
+  label: n,
+  color: '#ff0000',
+  size: s,
+  x: x,
+  y: y
+});
+
+	drawGraph();
+
 
 }
 
+
+
 //-----------------------DELEGATES
 
-$('#display_code').html(listNodes(nodes));
+$('#text_tab').html(listNodes(nodes));
+
+$('#btn_show_graph').click(function() {
+	drawGraph();
+});
 
 $('#btn_addnode').click(function() {
 
@@ -72,13 +117,13 @@ $('#btn_addnode').click(function() {
       var node_name = $('#field_node_name').val();
 
       addNode(xpos,ypos,size,node_id, node_name);
-      $('#display_code').html(listNodes(nodes));
+      $('#text_tab').html(listNodes(nodes));
+      drawGraph();
 
     });
 
 
 
-
-
 });
+
 
