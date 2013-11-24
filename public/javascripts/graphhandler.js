26f811433e06540e50a829cf7b0ec633c2e55cd3
Amman Vedi <amman.vedi@gmail.com>
2013-11-24 22:53:25 +0000

    added edge creation, added color picker for nodes. fisheye plugin functions.

--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -1,20 +1,29 @@
 console.log("started handler");
 
-
-
-
-
-
-
-
-
 $(document).ready(function() {
 
 	var sigRoot = document.getElementById('graph_canvas');
-var sigInst = sigma.init(sigRoot);
+var sigInst = sigma.init(sigRoot).drawingProperties({
+    defaultLabelColor: '#fff',
+    defaultLabelSize: 10,
+    defaultLabelBGColor: '#fff',
+    defaultLabelHoverColor: '#000',
+    labelThreshold: 8,
+    defaultEdgeType: 'curve'
+  }).graphProperties({
+    minNodeSize: 0.5,
+    maxNodeSize: 20,
+    minEdgeSize: 1,
+    maxEdgeSize: 3
+  }).mouseProperties({
+    maxRatio: 10
+  });
 
-
+  	var making_edge = false;
+  	var overnode = false;
+  	var last_node;
 	var mouseRoot = document.getElementById('sigma_mouse_1');
+	sigInst.bind('overnodes',showInfo).activateFishEye().draw();
 
 
 
@@ -39,17 +48,24 @@ var node = {
 }
 
 var nodes = new Array();
+var edge_path = new Array();
 
 
 //-----------------------FUNCTIONS
 
 
+//debug node set
+
+addNode(0.1,0.1,3,'a','node a', '#300000');
+addNode(0.2,0.2,3,'b','node b', '#300000');
+addNode(0.3,0.3,3,'c','node c', '#300000');
+
 
 function getMousePos(canvas, evt) {
         var rect = canvas.getBoundingClientRect();
         return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
+          x: evt.clientX,
+          y: evt.clientY
         };
       };
 
@@ -92,7 +108,9 @@ function listNodes(nodes){
 	return list;
 }
 
-function addNode(x, y, s, id, n){
+
+
+function addNode(x, y, s, id, n, c){
 	var newnode = {
 	posx: x,
 	posy: y,
@@ -105,16 +123,30 @@ function addNode(x, y, s, id, n){
 
 	sigInst.addNode(id,{
   label: n,
-  color: '#ff0000',
+  color: c,
   size: s,
   x: x,
   y: y
 });
 
+	//console.log('nodes');
+	//§console.log(nodes);
+
 	drawGraph();
 
 
 }
+
+function showInfo(event){
+  var node;
+      sigInst.iterNodes(function(n){
+        node = n;
+      },[event.content[0]]);
+      last_node = node;
+
+}
+
+
 
 
 
@@ -126,6 +158,27 @@ $('#btn_show_graph').click(function() {
 	drawGraph();
 });
 
+$('#btn_addedge').click(function() {
+
+if(making_edge){
+
+	for(var l = 0; l < edge_path.length-1; l++){
+		sigInst.addEdge(edge_path[l].id + edge_path[l+1].id,edge_path[l].id,edge_path[l+1].id);
+	}
+
+	
+
+	//process edge path
+	drawGraph();
+	edge_path = new Array();
+	making_edge = false;
+
+}else{
+	making_edge = true;
+}
+
+	});
+
 $('#btn_addnode').click(function() {
 
       var xpos = $('#field_node_xpos').val();
@@ -133,8 +186,9 @@ $('#btn_addnode').click(function() {
       var size = $('#field_node_size').val();
       var node_id = $('#field_node_id').val();
       var node_name = $('#field_node_name').val();
+      var node_color = $('#picker_edgecolor').val();
 
-      addNode(xpos,ypos,size,node_id, node_name);
+      addNode(xpos,ypos,size,node_id, node_name, node_color);
       $('#text_tab').html(listNodes(nodes));
       drawGraph();
 
@@ -142,13 +196,30 @@ $('#btn_addnode').click(function() {
 
 //-----------------------PERSISTANT LISTENERS
 
+mouseRoot.addEventListener('mousemove', function(evt) {
+
+
+
+	});
+
       mouseRoot.addEventListener('click', function(evt) {
         var mousePos = getMousePos(mouseRoot, evt);
         var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        console.log(message);
+        //console.log(message);
+
+        if (making_edge) {
+
+        	
+        	edge_path.push(last_node);
+        	console.log('edge spans');
+        	console.log(edge_path);
+
+        	//making_edge = false;
+        	overnode = false;
+        };
+
+
       }, false);
-
-
 
 });
 
