8e013f55893bd65dc46f30f31794756301d0a8af
Amman Vedi <amman.vedi@gmail.com>
2013-12-20 22:12:29 +0000

    added comments and cleaned code

--- public/javascripts/article_searcher.js
+++ public/javascripts/article_searcher.js
@@ -11,74 +11,54 @@
 
 $(document).ready(function() {
 
+		//hide the container of the search results
+		//its not needed when no results are being shown
+		$('#search_results_holder').height(0);
+		//this function is passed the searchterm 
+		function search(trm){
+		//re-extend the search results holder, to show the results to the user
+		$('#search_results_holder').height(300);
+		//use the httpget function to grab the custom google search
+		//JSON DATA
+		var json_dta = httpGet('https://www.googleapis.com/customsearch/v1?key=AIzaSyDM8_gZ-5DQVcBUt1y7qq_wAjUDbr4YSTA&cx=009521426283403904660:drg6vvs6o2a&q=' + trm);
+		//search results are stored in the 'items' array
+		var json_objects = jQuery.parseJSON( json_dta ).items;
 
-$('#search_results_holder').height(0);
+		json_objects.forEach(function(itm){
 
+			//for each result, add a corresponding search result division to the search
+			//results container
 
+			jQuery('<div/>', {
+				    class: 'search_result',
+			    	html: itm.title 
+					    	+ '<br/><br/>' 
+					    	+ itm.snippet 
+					    	+ '<br/><br/>' 
+					    	+ '<a href=\'http://' 
+					    	+ itm.formattedUrl 
+					    	+ '\'>view story</a>' 
+					    	+  '<br/><br/>'
 
-function search(trm){
-$('#search_results_holder').height(300);
-
-
-	var json_dta = httpGet('https://www.googleapis.com/customsearch/v1?key=AIzaSyDM8_gZ-5DQVcBUt1y7qq_wAjUDbr4YSTA&cx=009521426283403904660:drg6vvs6o2a&q=' + trm);
-var json_objects = jQuery.parseJSON( json_dta ).items;
-
-//console.log(json_objects);
-
-json_objects.forEach(function(itm){
-	console.log(itm.title);
-	console.log(itm.snippet);
-	console.log(itm.formattedUrl)
-	console.log('\n');
-
-
-jQuery('<div/>', {
-	    class: 'search_result',
-    html: itm.title + '<br/><br/>' + itm.snippet + '<br/><br/>' + '<a href=\'http://' + itm.formattedUrl + '\'>view story</a>' +  '<br/><br/>'
-}).appendTo('#search_results_holder');
-
-
-
-});
-
-
-
-
-
-}
-
-function buildURLs(searchterm){
-
-}
-
-//search id : 009521426283403904660:drg6vvs6o2a
-
-// api key : AIzaSyDM8_gZ-5DQVcBUt1y7qq_wAjUDbr4YSTA
-
-//base api : https://www.googleapis.com/customsearch/v1?key=AIzaSyDM8_gZ-5DQVcBUt1y7qq_wAjUDbr4YSTA&cx=009521426283403904660:drg6vvs6o2a&q=
-
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+			}).appendTo('#search_results_holder');
+			});
+		}
 
 
+		function httpGet(theUrl)
+		{
+		    var xmlHttp = null;
+		    xmlHttp = new XMLHttpRequest();
+		    xmlHttp.open( "GET", theUrl, false );
+		    xmlHttp.send( null );
+		    return xmlHttp.responseText;
+		}
 
-
-
-$('#btn_searcharticles').click(function(){
-
-  
-    search($('#input_searcharticles').val())
-
-});
-
-
+		//on click of the search button, take the input in the text
+		//field and pass it to the search function
+		$('#btn_searcharticles').click(function(){
+		    search($('#input_searcharticles').val())
+		});
 
 });
 
--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -1,367 +1,307 @@
-
-//embedly api key 365526c2e08d4ee5975adbb471147713
-
-console.log("started handler");
-
 $(document).ready(function() {
 
+          //create a socket to connect to the server
+          var socket = io.connect('http://localhost:3000');
+
+          //sigma.js preferences/options
+          var sigRoot = document.getElementById('graph_canvas');
+          var sigInst = sigma.init(sigRoot).drawingProperties({
+              defaultLabelColor: '#fff',
+              defaultLabelSize: 10,
+              defaultLabelBGColor: '#fff',
+              defaultLabelHoverColor: '#000',
+              labelThreshold: 20,
+              defaultEdgeType: 'curve'
+            }).graphProperties({
+              minNodeSize: 0.5,
+              maxNodeSize: 20,
+              minEdgeSize: 1,
+              maxEdgeSize: 3
+            }).mouseProperties({
+              maxRatio: 5,
+              minRatio: 1
+            });
+
+          //for the edge creation function, determines if an edge is 
+          //currently being drawn
+          var making_edge = false;
+          //boolean stores weather the mouse cursor is over a node in the graph
+          var overnode = false;
+          //stores the last node that the mouse cursor passed over, 
+          //when drawing edges
+          var last_node;
+          //when initialising an instance of the sigma.js graph, sigma will
+          //create a canvas dedicated to intercepting mouse events
+          //save this for later reference
+          var mouseRoot = document.getElementById('sigma_mouse_1');
+          //instantiate the graph, set the delegate for 'overnode' events
+          //applys fisheye plugin
+          sigInst.bind('overnodes',showInfo).activateFishEye().draw();
+
+
+          //initial jquery ui settings
+          $( "#tabs" ).tabs();
+          $("input[type=submit]").button();
+          $("#pallete").draggable({containment: "parent"});
+          $('#graph_tab').height($("body").height());
+          $('#text_tab').height(0);
+          $('.menu-link').bigSlide();
+
+          var meta = {};
+
+          //default node creation
+          var node = {
+          	posx: 100,
+          	posy: 100,
+          	size: 20,
+          	id: 'id',
+          	name: 'initial node',
+          }
+
+          //additional arrays to hold the nodes and edges
+          var nodes = new Array();
+          var edge_path = new Array();
+
+
+          //for generating debug graphs, retrieve a random hex color code
+          function get_random_color() {
+              var letters = '0123456789ABCDEF'.split('');
+              var color = '#';
+              for (var i = 0; i < 6; i++ ) {
+                  color += letters[Math.round(Math.random() * 15)];
+              }
+              return color;
+          }
+
+
+          //debug node set, generates 50 random nodes for testing purposes
+
+          for(var l = 0; l<50; l++){
+            addNode(Math.floor(Math.random()*20), Math.floor(Math.random()*20), 5,'node ' + l, ("this is node " + l).substring(0,17), get_random_color());
+          }
+
+
+          //reference nodes, thesea re used in calculation of the cartesian 
+          // mouse position
+          addNode(5,5,3, 'reference', 'reference', '#ffffff');
+          addNode(0,0,3, 'origin', 'origin', '#ffffff');
+
+
+          function getMousePos(canvas, evt) {
+                  var rect = canvas.getBoundingClientRect();
+                  return { x: evt.clientX, y: evt.clientY};
+          };
+
+          //draw the sigmajs instance
+          function drawGraph(){ sigInst.draw();}
+
+          //parse a node into a human readable form
+          function showNode(n){
+          	return '-   id: ' + n['id'] + ' name: ' + n['name'] + ' x: ' + n['posx'] + ' y: ' + n['posy'];
+          }
+
+          //list the nodes data 
+          function listNodes(nodes){
+          	var x = nodes.length;
+          	
+          	var list = '';
 
+          	if (x < 1) {return 'no nodes to list'};
 
+          	for (var i = 0; i < x; i++) {
+          		var nodeobj = nodes[i];
+          		list += showNode(nodeobj) + '<br/>'
+          	};
 
-  document.ontouchmove = function(event){
-    event.preventDefault();
-}
-
-document.ontouchstart = function(event){
-  event.preventDefault();
-}
+          	return list;
+          }
 
-//create a socket to connect to the server
-var socket = io.connect('http://localhost:3000');
 
+          //when a user wants to get a graph, use the socket connection to
+          //send a message to the server indicating which graph to pull
+          function initiatePullGraph(gID){
+            socket.emit('request_pullGraph', {graphID: gID});
+          }
 
-//sigma.js preferences
-var sigRoot = document.getElementById('graph_canvas');
-var sigInst = sigma.init(sigRoot).drawingProperties({
-    defaultLabelColor: '#fff',
-    defaultLabelSize: 10,
-    defaultLabelBGColor: '#fff',
-    defaultLabelHoverColor: '#000',
-    labelThreshold: 20,
-    defaultEdgeType: 'curve'
-  }).graphProperties({
-    minNodeSize: 0.5,
-    maxNodeSize: 20,
-    minEdgeSize: 1,
-    maxEdgeSize: 3
-  }).mouseProperties({
-    maxRatio: 5,
-    minRatio: 1
-  });
+          //once a graph has been pulled from the server, it is neccacary to 
+          //rebuild it into a valid sigma graph 
+          function rebuildPullGraph(pulled){
+            //TO DO
+              //add all nodes and edges back to the sigmajs instance
+          }
 
-var making_edge = false;
-var overnode = false;
-var last_node;
-var mouseRoot = document.getElementById('sigma_mouse_1');
+          //add a node to the graph
+          function addNode(x, y, s, id, n, c){
 
-//instantiate the graph
-sigInst.bind('overnodes',showInfo).activateFishEye().draw();
+          //node attributes for debugging
+            var attribs = [
+              {name: 'link', val:'http://www.google.com/'},
+              {name: 'description', val:'this is the description of the node'}];
 
+          //node to push to additional node array
+          	var newnode = {
+          	posx: x,
+          	posy: y,
+          	size: s,
+          	id: id,
+          	name: n,
+          	}
 
-//initial jquery ui settings
-$( "#tabs" ).tabs();
-$("input[type=submit]").button();
-$("#pallete").draggable({containment: "parent"});
-//expand the graph tab initally
-$('#graph_tab').height($("body").height());
-//hide the text tab initially
-$('#text_tab').height(0);
-$('.menu-link').bigSlide();
-//-----------------------VARIABLES
+          	nodes.push(newnode);
 
-var meta = {};
+            //add node to the graph instance
+          	sigInst.addNode(id,{
+            label: n,
+            color: c,
+            size: s,
+            x: x,
+            y: y,
+            attr: attribs
+          });
 
-//default node creation
-var node = {
-	posx: 100,
-	posy: 100,
-	size: 20,
-	id: 'id',
-	name: 'initial node',
-}
-
-//additional arrays to hold the nodes and edges
-var nodes = new Array();
-var edge_path = new Array();
-
-
-//-----------------------FUNCTIONS
-
-function get_random_color() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
-}
-
-
-//debug node set, generates 50 random nodes for testing purposes
-
-for(var l = 0; l<50; l++){
-  addNode(Math.floor(Math.random()*20), Math.floor(Math.random()*20), 5,'node ' + l, ("this is node " + l).substring(0,17), get_random_color());
-}
-
-
-//reference nodes, thesea re used in calculation of the cartesian 
-// mouse position
-addNode(5,5,3, 'reference', 'reference', '#ffffff');
-addNode(0,0,3, 'origin', 'origin', '#ffffff');
+            //redraw the graph after node addition
+          	drawGraph();
 
-/*
-addNode(0.1,0.1,3,'a','node a', '#300000');
-addNode(0.2,0.2,3,'b','node b', '#300000');
-addNode(0.3,0.3,3,'c','node c', '#300000');*/
+          }
 
+          //function called when the mouse is over a node
+          function showInfo(event){
+            var node;
+                sigInst.iterNodes(function(n){
+                      node = n;
+                      },[event.content[0]]);
+                //save this node as the last node hovered over
+                last_node = node;
 
-function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX,
-          y: evt.clientY
-        };
-      };
+                //show details in console
+                console.log(node['attr']['attr'][0]['name'] + ' : ' + node['attr']['attr'][0]['val']);
+                console.log(node['attr']['attr'][1]['name'] + ' : ' + node['attr']['attr'][1]['val']);
+                console.log('node centre xy ' + node['x'] + ' ' +  node['y']);
 
 
+          }
 
-function drawGraph(){ sigInst.draw();}
 
-//parse a node into a human readable form
-function showNode(n){
-	return '-   id: ' + n['id'] + ' name: ' + n['name'] + ' x: ' + n['posx'] + ' y: ' + n['posy'];
-}
 
-//list the nodes data 
-function listNodes(nodes){
-	var x = nodes.length;
-	
-	var list = '';
 
-	if (x < 1) {return 'no nodes to list'};
 
-	for (var i = 0; i < x; i++) {
-		var nodeobj = nodes[i];
-		list += showNode(nodeobj) + '<br/>'
-	};
+          //-----------------------DELEGATES
 
-	return list;
-}
+          $('#text_tab').html(listNodes(nodes));
 
-function initiatePullGraph(gID){
-  socket.emit('request_pullGraph', {graphID: gID});
-}
+          $('#btn_show_graph').click(function() {
+              $('#text_tab').height(0);
+              $('#graph_tab').height($("body").height());
+              drawGraph();
+          });
 
-function rebuildPullGraph(pulled){
-  //TO DO
-}
+          $('#btn_addedge').click(function() {
 
-//add a node to the graph
-function addNode(x, y, s, id, n, c){
+            if(making_edge){
 
-//attributes for debugging
-  var attribs = [
-    {name: 'link', val:'http://www.google.com/'},
-    {name: 'description', val:'this is the description of the node'}
+              	for(var l = 0; l < edge_path.length-1; l++){
+              		sigInst.addEdge(edge_path[l].id + edge_path[l+1].id,edge_path[l].id,edge_path[l+1].id);
+              	}
 
-]
+              	drawGraph();
+              	edge_path = new Array();
+              	making_edge = false;
 
-//node to push to additional node array
-	var newnode = {
-	posx: x,
-	posy: y,
-	size: s,
-	id: id,
-	name: n,
-	}
+            }else{
+              	making_edge = true;
+            }
 
-	nodes.push(newnode);
-  //add node to the graph instance
-	sigInst.addNode(id,{
-  label: n,
-  color: c,
-  size: s,
-  x: x,
-  y: y,
-  attr: attribs
-});
+          });
 
-	drawGraph();
 
-}
+          $('#btn_show_text').click(function() {
+              $('#text_tab').height(100);
+              $('#graph_tab').height(0);
+            }
+          );
 
-//function called when the mouse is over a node
-function showInfo(event){
-  var node;
-      sigInst.iterNodes(function(n){
-        node = n;
-      },[event.content[0]]);
-      last_node = node;
-      console.log(node['attr']['attr'][0]['name'] + ' : ' + node['attr']['attr'][0]['val']);
-      console.log(node['attr']['attr'][1]['name'] + ' : ' + node['attr']['attr'][1]['val']);
-      console.log('node centre xy ' + node['x'] + ' ' +  node['y']);
+          $('#graph_canvas').click(function(evnt) {
 
+                  //calculating the sigmajs cartesian co-ordinates canvas co-ords
+                  var ratio_display_x = ((sigInst.getNodes('reference').x - sigInst.getNodes('origin').x) / (sigInst.getNodes('reference').displayX - sigInst.getNodes('origin').displayX));
+                  var ratio_display_y = ((sigInst.getNodes('reference').y - sigInst.getNodes('origin').y) / (sigInst.getNodes('reference').displayY - sigInst.getNodes('origin').displayY));
+                  var out_x = (evnt.clientX - sigInst.getNodes('origin').displayX) * ratio_display_x;
+                  var out_y = (evnt.clientY - sigInst.getNodes('origin').displayY) * ratio_display_y;
 
-}
+                  $('#field_node_xpos').val(out_x.toString().substring(0,4));
+                  $('#field_node_ypos').val(out_y.toString().substring(0,4));
+              }
+          );
 
+          $('#btn_savegraph').click(function() {
 
+                var nd = new Array();
+                var ed = new Array();
 
+                var n = sigInst.iterNodes(function(nds){ nd.push(nds.id); });
+                var e = sigInst.iterEdges(function(egs){ ed.push(egs.id); });
 
+                var to_send_nodes ={nodes: sigInst.getNodes(nd)};
+                var to_send_edges = {edges: sigInst.getNodes(ed)};
 
-//-----------------------DELEGATES
+                console.log(socket);
+                console.log('client socket ' + socket.id + ' sending ' + to_send_nodes + ' ' + to_send_edges + ' to server');
+                console.log(to_send_nodes)
+                console.log(to_send_edges)
 
-$('#text_tab').html(listNodes(nodes));
+                socket.emit('savegraph_nodes', to_send_nodes);
+                socket.emit('savegraph_edges', to_send_edges);
 
-$('#btn_show_graph').click(function() {
 
-$('#text_tab').height(0);
-$('#graph_tab').height($("body").height());
-	drawGraph();
-});
+          });
 
-$('#btn_addedge').click(function() {
 
-if(making_edge){
 
-	for(var l = 0; l < edge_path.length-1; l++){
-		sigInst.addEdge(edge_path[l].id + edge_path[l+1].id,edge_path[l].id,edge_path[l+1].id);
-	}
+          $('#btn_addnode').click(function() {
 
-	
+                var xpos = $('#field_node_xpos').val();
+                var ypos = $('#field_node_ypos').val();
+                var size = $('#field_node_size').val();
+                var node_id = $('#field_node_id').val();
+                var node_name = $('#field_node_name').val();
+                var node_color = $('#picker_edgecolor').val();
+                addNode(xpos,ypos,size,node_id, node_name, node_color);
+                $('#text_tab').html(listNodes(nodes));
+                drawGraph();
+                var nd = sigInst.getNodes(node_id);
+                sigInst.goTo(nd['displayX'],nd['displayY'],2);
 
-	//process edge path
-	drawGraph();
-	edge_path = new Array();
-	making_edge = false;
+          });
 
-}else{
-	making_edge = true;
-}
+          //-----------------------PERSISTANT LISTENERS
 
-	});
+          mouseRoot.addEventListener('mousemove', function(evt) {});
 
-mouseRoot.addEventListener('mousemove', function(evt) {
+          mouseRoot.addEventListener('click', function(evt) {
 
+                  var mousePos = getMousePos(mouseRoot, evt);
+                  var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
 
+                  if (making_edge) {
+                    	edge_path.push(last_node);
+                    	console.log('edge spans');
+                    	console.log(edge_path);
+                    	overnode = false;
+                  };
 
-  });
+          }, false);
 
-$('#btn_show_text').click(function() {
+                //-------SOCKET HANDLERS
 
-$('#text_tab').height(100);
-$('#graph_tab').height(0);
+          socket.on('hs_id', function (data) {
+            socket.id = data.data;
+            console.log('client : recieved id ' + socket.id + ' from server');
+          });
 
-}
-);
-
-$('#graph_canvas').click(function(evnt) {
-
-
-var ratio_display_x = ((sigInst.getNodes('reference').x - sigInst.getNodes('origin').x) / (sigInst.getNodes('reference').displayX - sigInst.getNodes('origin').displayX));
-var ratio_display_y = ( (sigInst.getNodes('reference').y - sigInst.getNodes('origin').y) / (sigInst.getNodes('reference').displayY - sigInst.getNodes('origin').displayY));
-
-
-var out_x = (evnt.clientX - sigInst.getNodes('origin').displayX) * ratio_display_x;
-var out_y = (evnt.clientY - sigInst.getNodes('origin').displayY) * ratio_display_y;
-
-
-console.log('x: ' + out_x + ' y: ' + out_y);
-
-  $('#field_node_xpos').val(out_x.toString().substring(0,4));
-  $('#field_node_ypos').val(out_y.toString().substring(0,4));
-
-}
-
-
-);
-
-$('#btn_savegraph').click(function() {
-
-var nd = new Array();
-var ed = new Array();
-
-  var n = sigInst.iterNodes(function(nds){
-        nd.push(nds.id);
-  });
-
-  var e = sigInst.iterEdges(function(egs){
-        ed.push(egs.id);
-  });
-
-var to_send_nodes ={nodes: sigInst.getNodes(nd)};
-var to_send_edges = {edges: sigInst.getNodes(ed)};
-
-
-
-console.log(socket);
-console.log('client socket ' + socket.id + ' sending ' + to_send_nodes + ' ' + to_send_edges + ' to server');
-console.log(to_send_nodes)
-console.log(to_send_edges)
-
-
-socket.emit('savegraph_nodes', to_send_nodes);
-socket.emit('savegraph_edges', to_send_edges);
-
-initiatePullGraph("123");
-
-
-  });
-
-
-
-$('#btn_addnode').click(function() {
-
-      var xpos = $('#field_node_xpos').val();
-      var ypos = $('#field_node_ypos').val();
-      var size = $('#field_node_size').val();
-      var node_id = $('#field_node_id').val();
-      var node_name = $('#field_node_name').val();
-      var node_color = $('#picker_edgecolor').val();
-
-      addNode(xpos,ypos,size,node_id, node_name, node_color);
-      $('#text_tab').html(listNodes(nodes));
-      drawGraph();
-
-      var nd = sigInst.getNodes(node_id);
-
-      sigInst.goTo(nd['displayX'],nd['displayY'],2);
-
-    });
-
-//-----------------------PERSISTANT LISTENERS
-
-mouseRoot.addEventListener('mousemove', function(evt) {
-
-
-
-	});
-
-      mouseRoot.addEventListener('click', function(evt) {
-        var mousePos = getMousePos(mouseRoot, evt);
-        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        //console.log(message);
-
-        if (making_edge) {
-
-        	edge_path.push(last_node);
-        	console.log('edge spans');
-        	console.log(edge_path);
-
-        	//making_edge = false;
-        	overnode = false;
-        };
-
-
-      }, false);
-
-      //-------SOCKET HANDLERS
-
-socket.on('hs_id', function (data) {
-
-  socket.id = data.data;
-  console.log('client : recieved id ' + socket.id + ' from server');
+          socket.on('request_pullGraph_success', function (data){
+            console.log('client : pullGraph request successful, result:')
+            console.log(data.data);
+            rebuildPullGraph(data.data);
+          });
 
 });
-
-socket.on('request_pullGraph_success', function (data){
-  console.log('client : pullGraph request successful, result:')
-  console.log(data.data);
-  rebuildPullGraph(data.data);
-});
-
-});
-
-
-
-
-
