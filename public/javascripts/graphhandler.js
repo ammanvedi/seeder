77f1f622fb7870e3769078e70cc459212cccd0ff
Amman Vedi <amman.vedi@gmail.com>
2014-02-25 15:36:46 +0000

    webhook auto-deploy EC2 test [redeploy]

--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -257,7 +257,7 @@ $(document).ready(function () {
 
 
     //def
-    addnodePREFS['name'] = 'default name';
+    addnodePREFS['name'] = 'cool name';
     addnodePREFS['text'] = 'lorem ipsum';
     addnodePREFS['link'] = 'http://www.google.com/';
     addnodePREFS['TYPE'] = 'TEST';
