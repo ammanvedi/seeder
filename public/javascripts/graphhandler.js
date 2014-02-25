095ab43dac952f0a4b7e9356caf9a1eb8adbf423
Amman Vedi <amman.vedi@gmail.com>
2014-02-25 16:55:04 +0000

    yup [redeploy]

--- public/javascripts/graphhandler.js
+++ public/javascripts/graphhandler.js
@@ -257,7 +257,7 @@ $(document).ready(function () {
 
 
     //def
-    addnodePREFS['name'] = 'cooler yet name';
+    addnodePREFS['name'] = 'start here';
     addnodePREFS['text'] = 'lorem ipsum';
     addnodePREFS['link'] = 'http://www.google.com/';
     addnodePREFS['TYPE'] = 'TEST';
