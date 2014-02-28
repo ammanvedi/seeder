0a32a53614e2c25f950d28644bc61b24e56a29a6
Amman Vedi <amman.vedi@gmail.com>
2014-02-28 17:12:59 +0000

    [redeploy] updated explore interface

--- public/javascripts/explorehandler.js
+++ public/javascripts/explorehandler.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 var  MainSidebarExplore = $('.ui.sidebar');
 
-MainSidebarExplore.sidebar('toggle');
+//MainSidebarExplore.sidebar('toggle');
 MainSidebarExplore.sidebar('attach events','#sidebar-toggle' , 'toggle');
 
 console.log('lol');
