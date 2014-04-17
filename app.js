635b40e18b8294c8e114b67ca13096d2efc7371e
Amman Vedi <amman.vedi@gmail.com>
2014-04-17 16:57:27 +0100

    [redeploy] changed google auth callback uri to seeder.co

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ MongoClient.connect("mongodb://ammanvedi:poopoo12@ds057528.mongolab.com:57528/se
 });
 
 passport.use(new GoogleStrategy({
-    returnURL: 'http://54.201.24.162/auth/google/return',
-    realm: 'http://54.201.24.162/'
+    returnURL: 'http://seeder.co/auth/google/return',
+    realm: 'http://seeder.co/'
   },
   function(identifier, profile, done) {
   
